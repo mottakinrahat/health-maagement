520ca8f79777c1c6b575e2f75061e024dc55b60e
mottakinrahat <rahatchowhdury661@gmail.com>
2026-02-07 23:13:38 +0600

    upload

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -2,11 +2,21 @@ import express from "express";
 import { UserController } from "./user.controller";
 import { auth } from "../../middleWares/auth";
 import { UserRole } from "../../../../generated/prisma";
+import multer from "multer";
 
 
 const router = express.Router();
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
 
+const upload = multer({ storage: storage })
 
 router.post("/",auth(UserRole.ADMIN,UserRole.SUPER_ADMIN),  UserController.createAdminUser);//
 router.get("/",auth(UserRole.ADMIN,UserRole.SUPER_ADMIN), UserController.createAdminUser);
